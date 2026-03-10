19c842b2487ec00d67e094b7b6b8479914650362
Quang Anh Pham <anhpq.dev@gmail.com>
2026-03-10 11:16:42 +0700

    fix: Sửa lỗi typo theo review của bot

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -3,9 +3,9 @@ import { SafeAreaView, View, Text } from "react-native";
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff7f7" }}>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f3e6e6" }}>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center", padding: 24 }}>
-        <Text style={{ fontSize: 24, fontWeight: "700", marginBottom: 8, color: "#819ed7" }}>Mobile App Baseeeeee</Text>
+        <Text style={{ fontSize: 24, fontWeight: "700", marginBottom: 8, color: "#819ed7" }}>Mobile App Base</Text>
         <Text style={{ textAlign: "center", color: "#0f40a1" }}>
           Clean Expo base.
         </Text>
